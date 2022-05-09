81a09b77e5fc222c45d5f2d94b143d4e8a9a276f
jasongwq <jasongwq@gmail.com>
2022-05-09 14:30:25 +0800

    Update SearchEngineClient.java

--- src/main/java/org/codelibs/fess/es/client/SearchEngineClient.java
+++ src/main/java/org/codelibs/fess/es/client/SearchEngineClient.java
@@ -195,7 +195,7 @@ public class SearchEngineClient implements Client {
 
     protected int sizeForUpdate = 100;
 
-    protected String scrollForUpdate = "1m";
+    protected String scrollForUpdate = "5m";
 
     protected int maxConfigSyncStatusRetry = 10;
 
